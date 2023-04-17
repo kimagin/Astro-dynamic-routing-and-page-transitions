b6505c41d945c6d6cc0bbb01c9a1196342daaf74
kimagin <info@kimagination.net>
2023-04-17 17:34:17 +0200

    Initializing the  application

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import Swup from 'swup'
+import SwupHeadPlugin from '@swup/head-plugin'
+import SwupSlideTheme from '@swup/slide-theme'
+import SwupOverlayTheme from '@swup/overlay-theme'
+import SwupFadeTheme from '@swup/fade-theme'
+
+const swup = new Swup({
+  plugins: [],
+})
